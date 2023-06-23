071c529a972dcd8409d9d03cb4def94834cc8219
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2023-06-23 22:52:25 +0100

    commit1

---
+++ test.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS test;
+
+-- Path: test.sql
+
+USE test;
+
+-- Create table test.test
+CREATE TABLE IF NOT EXISTS student_user (
+    id         INT AUTO_INCREMENT   PRIMARY KEY,
+    username   VARCHAR(50)          NOT NULL,
+    password   VARCHAR(255)         NOT NULL,
+    email      VARCHAR(255)         NOT NULL,
+    CONSTRAINT Email UNIQUE (email),
+    CONSTRAINT Username UNIQUE (username)
+);
+
+
+# SELECT * FROM student_user;

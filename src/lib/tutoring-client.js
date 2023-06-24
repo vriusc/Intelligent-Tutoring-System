5c789cf4af869629558339b56dd4a51936994977
Ubuntu <ubuntu@ip-172-31-91-156.ec2.internal>
2023-06-24 21:04:32 +0000

    dnschange

--- src/lib/tutoring-client.js
+++ src/lib/tutoring-client.js
@@ -3,25 +3,25 @@ import axios from 'axios'
 //export const baseURL = 'http://localhost:8080'
 
 export const loginStudent = async (loginForm) => {
-  return await axios.post(`/studentUser/login`, loginForm)
+  return await axios.post(`/api/studentUser/login`, loginForm)
 }
 
 export const registerStudent = async (studentForm) => {
-  return await axios.post(`/studentUser/register`, studentForm)
+  return await axios.post(`/api/studentUser/register`, studentForm)
 }
 
 export const updatePassword = async (updateStudentForm) => {
-  return await axios.post(`/studentUser/password`, updateStudentForm)
+  return await axios.post(`/api/studentUser/password`, updateStudentForm)
 }
 
 export const getStudent = async (studentId) => {
-  return await axios.get(`/studentUser/${studentId}`)
+  return await axios.get(`/api/studentUser/${studentId}`)
 }
 
 export const getAllSubjects = async (params) => {
-  return await axios.get(`/subjects/queryAllSubjects`, { params })
+  return await axios.get(`/api/subjects/queryAllSubjects`, { params })
 }
 
 export const getSubjectsById = async (studentId) => {
-  return await axios.get(`/studentSubjects/queryByStudentId/${studentId}`)
+  return await axios.get(`/api/studentSubjects/queryByStudentId/${studentId}`)
 }

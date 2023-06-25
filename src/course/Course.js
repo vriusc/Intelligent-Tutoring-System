1079392401bbf77c871be290d911defe2e117174
Alvaro Patty Cruz <alvaro.patty@ucdconnect.ie>
2023-06-25 18:40:57 +0100

    course list finished

--- src/course/Course.js
+++ src/course/Course.js
@@ -1,7 +1,10 @@
+import './Course.css'
 import { Navigate, useLoaderData } from 'react-router-dom'
 import { getStudent, getUnitsBySubjectId } from '../lib/tutoring-client'
 import { useEffect, useState } from 'react'
 import Header from '../element/Header'
+import { Button, Card, CardBody, CardText, CardTitle } from 'reactstrap'
+import { BsFillCheckCircleFill } from 'react-icons/bs'
 
 export async function loader({ params }) {
   const units = await getUnitsBySubjectId(params.courseId)
@@ -34,7 +37,33 @@ const Course = () => {
     <>
       <Header user={student} subjectCount={1} title="My units" />
       <div className="Course-container">
-        <div className="Course"></div>
+        <div className="Course">
+          <h3>Units list</h3>
+          <div className="Units-list">
+            {data.map((unit, index) => (
+              <Card key={unit.unitId} className="mt-3">
+                <CardBody className="Unit-list-body p-4">
+                  <div className="">
+                    <CardTitle tag="h5" className="mb-2">
+                      {unit.unitName}
+                    </CardTitle>
+                    <CardText className="text-muted">{unit.description}</CardText>
+                  </div>
+                  {/* TODO if the unit is finished set an Icon done */}
+                  <div style={{ alignSelf: 'center' }}>
+                    {index === 0 ? (
+                      <h4 style={{ color: 'green' }}>
+                        Finished <BsFillCheckCircleFill />
+                      </h4>
+                    ) : (
+                      <Button color="dark">START</Button>
+                    )}
+                  </div>
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )

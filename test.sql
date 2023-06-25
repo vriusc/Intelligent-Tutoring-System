5a0fb5435a014c373ac7d9d3445800ea5eba13c1
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2023-06-25 22:37:04 +0100

    create new table materials

--- test.sql
+++ test.sql
@@ -22,3 +22,16 @@ CREATE TABLE Materials (
     Materials_ID INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each material
     File_Path VARCHAR(255) NOT NULL-- Path to the file
 );
+
+-- CREATE TABLE Listening_Judgement_By_Picture - to see if the picture matches the audio
+CREATE TABLE Listening_Judgement_By_Picture (
+    Listening_Judgement_By_Picture_ID INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each question
+    Question VARCHAR(255) NOT NULL,      -- Description of the question Like "Does the picture match the audio?"
+    Picture_File_Path INT NOT NULL,     -- Picture_File_Path
+    Audio_File_Path INT NOT NULL, -- Path to the Audio
+    Answer ENUM ('True', 'False') NOT NULL,    -- Answer
+    Level ENUM ('HSK1', 'HSK2', 'HSK3', 'HSK4', 'HSK5', 'HSK6') NOT NULL, -- HSK level
+    Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Date of creation
+    FOREIGN KEY (Picture_File_Path) REFERENCES Materials(Materials_ID),
+    FOREIGN KEY (Audio_File_Path) REFERENCES Materials(Materials_ID)
+);

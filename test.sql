719cd05199aa06194fc7c9b2ebcb10fd8a8a5c57
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2023-06-25 22:25:42 +0100

    commit1

--- test.sql
+++ test.sql
@@ -4,12 +4,15 @@ CREATE SCHEMA IF NOT EXISTS test;
 
 USE test;
 
--- Create table for user(student)
-CREATE TABLE IF NOT EXISTS student_user (
-    id         INT AUTO_INCREMENT   PRIMARY KEY,
-    username   VARCHAR(50)          NOT NULL,
-    password   VARCHAR(255)         NOT NULL,
-    email      VARCHAR(255)         NOT NULL,
-    CONSTRAINT Email UNIQUE (email),
-    CONSTRAINT Username UNIQUE (username)
+-- create table student-info
+CREATE TABLE Users (
+    Id INT AUTO_INCREMENT, -- Unique ID for each user
+    Username VARCHAR(50) NOT NULL, -- Unique username
+    Password VARCHAR(255) NOT NULL,  -- Hashed password
+    Email VARCHAR(255) NOT NULL,    -- Unique email
+    Level ENUM ('HSK1', 'HSK2', 'HSK3', 'HSK4', 'HSK5', 'HSK6') NOT NULL, -- HSK level
+    Study_Time INT NOT NULL, -- Total study time in seconds
+    PRIMARY KEY(id),    -- Set id as primary key
+    UNIQUE(username),  -- Set username as unique
+    UNIQUE(email)  -- Set email as unique
 );

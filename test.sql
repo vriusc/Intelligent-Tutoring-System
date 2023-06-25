fcf1d4682474ea4489fd896a49cd255cf6201e47
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2023-06-25 22:58:00 +0100

    create new table Listen_Choice_Right_Word change description of table Listening_Choice_By_Picture

--- test.sql
+++ test.sql
@@ -23,7 +23,7 @@ CREATE TABLE Materials (
     File_Path VARCHAR(255) NOT NULL-- Path to the file
 );
 
--- CREATE TABLE Listening_Judgement_By_Picture - check if the picture matches the audio
+-- CREATE TABLE Listening_Judgement_By_Picture - to see if the picture matches the audio
 CREATE TABLE Listening_Judgement_By_Picture (
     Listening_Judgement_By_Picture_ID INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each question
     Question VARCHAR(255) NOT NULL,      -- Description of the question Like "Does the picture match the audio?"
@@ -33,5 +33,29 @@ CREATE TABLE Listening_Judgement_By_Picture (
     Level ENUM ('HSK1', 'HSK2', 'HSK3', 'HSK4', 'HSK5', 'HSK6') NOT NULL, -- HSK level
     Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Date of creation
     FOREIGN KEY (Picture_File_Path) REFERENCES Materials(Materials_ID),
+    FOREIGN KEY (Audio_File_Path) REFERENCES Materials(Materials_ID)
+);
+
+-- CREATE TABLE Listening_Choice_By_Picture - choose the option if the picture matches the audio
+CREATE TABLE Listening_Choice_By_Picture (
+    Listening_Choice_By_Picture_ID INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each question
+    Question VARCHAR(255) NOT NULL,    -- Description of the question Like "Choose the options that correspond to the audio"
+    Picture_File_Path INT NOT NULL,     -- Picture_File_Path
+    Audio_File_Path INT NOT NULL, -- Path to the Audio
+    Answer ENUM ('A', 'B', 'C', 'E', 'F', 'G') NOT NULL,    -- Answer
+    Level ENUM ('HSK1', 'HSK2', 'HSK3', 'HSK4', 'HSK5', 'HSK6') NOT NULL, -- HSK level
+    Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  -- Date of creation
+    FOREIGN KEY (Picture_File_Path) REFERENCES Materials(Materials_ID),
+    FOREIGN KEY (Audio_File_Path) REFERENCES Materials(Materials_ID)
+);
+
+
+-- CREATE TABLE Listen_Choice_Right_Word - choose the option if the description matches the audio
+CREATE TABLE Listen_Choice_Right_Word (
+    Listen_Choice_Right_Word_ID INT AUTO_INCREMENT PRIMARY KEY,   -- Unique ID for each question
+    Question VARCHAR(255) NOT NULL,      -- Description of the question Like "Choose corresponding words to the audio"
+    Audio_File_Path INT NOT NULL, -- Path to the Audio
+    Level ENUM ('HSK1', 'HSK2', 'HSK3', 'HSK4', 'HSK5', 'HSK6') NOT NULL, -- HSK level
+    Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  -- Date of creation
     FOREIGN KEY (Audio_File_Path) REFERENCES Materials(Materials_ID)
 );

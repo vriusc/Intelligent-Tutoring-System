0d894fb7fdb5c344c936f6b1565bd751483797b5
xiaochili <129189371+xiaochili@users.noreply.github.com>
2023-07-30 17:28:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ def your_route_function():
 
 # Run the Flask app
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)

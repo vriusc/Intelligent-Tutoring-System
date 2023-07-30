eb4ae689b0ba2db9fcdecab314c046d316a39462
xiaochili <129189371+xiaochili@users.noreply.github.com>
2023-07-30 21:45:27 +0100

    Update tutoring-client.js

--- src/lib/tutoring-client.js
+++ src/lib/tutoring-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'http://localhost:8080/api'
+export const baseURL = '/api'
 
 export const loginStudent = async (loginForm) => {
   return await axios.post(`${baseURL}/studentUser/login`, loginForm)

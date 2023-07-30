e6bba98ca8c6f2dd79e9cbf4e09969e47f90f98e
vriusc <qianyongru@icloud.com>
2023-07-30 23:09:49 +0100

    commit by qianyongru

--- src/main/java/com/example/demo/controller/StudentUnitsController.java
+++ src/main/java/com/example/demo/controller/StudentUnitsController.java
@@ -17,7 +17,7 @@ import javax.annotation.Resource;
  */
 //@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("api/studentUnits")
+@RequestMapping("/api/studentUnits")
 public class StudentUnitsController {
     /**
      * 服务对象

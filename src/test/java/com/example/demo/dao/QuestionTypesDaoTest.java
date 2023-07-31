370eea849e47771dd4a9f01fcac9675797550f0f
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2023-07-31 21:05:07 +0100

    joy commit

--- src/test/java/com/example/demo/dao/QuestionTypesDaoTest.java
+++ src/test/java/com/example/demo/dao/QuestionTypesDaoTest.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
